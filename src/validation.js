57192a269af046895b0b679973790219637bdc35
sbornia <sebastianbornia@gmail.com>
2022-01-10 16:43:09 -0300

    Validate that valid role is provided

--- src/validation.js
+++ src/validation.js
@@ -1,3 +1,5 @@
+const VALID_ROLES = ['USER', 'ADMIN', 'STAFF']
+
 export function validateClientPayload({ user, business }) {
   if (!user) {
     throw new Error("user payload is required");
@@ -8,6 +10,11 @@ export function validateClientPayload({ user, business }) {
   if (!user.id || !user.email || !user.name) {
     throw new Error(
       "user payload must contain the following properties: id, email, name"
+    );
+  }
+  if (VALID_ROLES.indexOf(user.role) === -1) {
+    throw new Error(
+      `user payload must contain the role property, with one of the following values: ${VALID_ROLES.join(', ')}`
     );
   }
   if (!business.id || !business.name) {
