f6259f4e0b0c6b3d254445c69d1d89101b137b99
sbornia <sebastianbornia@gmail.com>
2022-01-15 16:46:34 -0300

    Sort payload to normalize signature computation across apps

--- src/signature.ts
+++ src/signature.ts
@@ -9,13 +9,23 @@ export function signatureFor({ apiKey, payload, timestamp }) {
     throw new Error("apiKey is required");
   }
 
-  const data = {
+  // Attach timestamp and sort object keys
+  const data = sortedKeys({
     ...payload,
     timestamp,
-  };
+  });
 
   return crypto
     .createHmac("sha256", apiKey)
     .update(JSON.stringify(data))
     .digest("hex");
 }
+
+const sortedKeys = (obj: any) => {
+  return Object.keys(obj)
+    .sort()
+    .reduce(function (acc: any, key) {
+      acc[key] = obj[key];
+      return acc;
+    }, {});
+};
